c1d0e758199a48e211e8944938a2545b77b69196
Vladislav Kozhevnikov <vladislavkoj@gmail.com>
2017-05-19 03:07:30 +0300

    Test Land Пол дела позади

---
+++ js/test.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $('#fullpage').fullpage({
+    anchors: ['firstPage', 'secondPage', '3rdPage'],
+    navigation: true,
+    navigationPosition: 'right',
+    navigationTooltips: ['Старт', 'Задачи', 'Проекты', 'Детали', 'Анимации', 'Дизайн', 'Главная', 'Контакты'],
+    responsiveWidth: 900,
+    responsiveHeight: 885,
+    afterResponsive: function(isResponsive) {
+
+    }
+
+  });
+});
